4e9003fd1bcfb2e7455c15c191b0ee4a71f8fcfa
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-12-13 22:05:54 +0530

    adding mvc and using https filter

--- Confaque/Startup.cs
+++ Confaque/Startup.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Confaque
@@ -15,6 +12,8 @@ namespace Confaque
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // configures mvc with a require https filter.
+            services.AddMvc().AddMvcOptions(option => option.Filters.Add(new RequireHttpsAttribute()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
