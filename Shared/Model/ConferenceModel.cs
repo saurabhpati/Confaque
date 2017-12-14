f17bfe36fccbced123ad0349444a1c7f81c5f1cd
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-12-14 22:13:50 +0530

    Adding a shared project

---
+++ Shared/Model/ConferenceModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Shared.Models
+{
+    public class ConferenceModel
+    {
+        public ConferenceModel()
+        {
+            this.Start = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public string Location { get; set; }
+
+        public int AttendeeTotal { get; set; }
+    }
+}
