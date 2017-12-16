1eb0f55753a2fe3ddf067dcd29b1d641e66ddc0a
Saurabh Pati <saurabhpati.pati@gmail.com>
2017-12-16 22:08:40 +0530

    Adding conference and proposal controllers

---
+++ Confaque/Controllers/ConferenceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confaque.Service;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Confaque.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly IConferenceService _service;
+
+        public ConferenceController(IConferenceService service)
+        {
+            this._service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Conference Overview";
+            return View(await this._service.GetAll().ConfigureAwait(false));
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            ViewBag.Title = "Add Conference";
+            return await Task.FromResult<IActionResult>(View(new ConferenceModel())).ConfigureAwait(false);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                throw new InvalidOperationException("Model State is invalid");
+            }
+
+            await this._service.Add(model).ConfigureAwait(false);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Confaque/Controllers/ProposalController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confaque.Service;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Confaque.Controllers
+{
+    public class ProposalController : Controller
+    {
+        private readonly IProposalService _service;
+
+        public ProposalController(IProposalService service)
+        {
+            this._service = service;
+        }
+
+        public async Task<IActionResult> Index(int conferenceId)
+        {
+            ViewBag.ConferenceId = conferenceId;
+            ViewBag.Title = "Proposals for the selected conference";
+            return View(await this._service.GetAll(conferenceId).ConfigureAwait(false));
+        }
+
+        public async Task<IActionResult> Add(int conferenceId)
+        {
+            ViewBag.Title = "Add Proposal";
+            return await Task.FromResult<IActionResult>(View(new ProposalModel()
+            {
+                ConferenceId = conferenceId
+            })).ConfigureAwait(false);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProposalModel proposal)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new InvalidOperationException("Model state is invalid");
+            }
+
+            await this._service.Add(proposal).ConfigureAwait(false);
+            return RedirectToAction("Index", new { conferenceId = proposal.ConferenceId });
+        }
+
+        public async Task<IActionResult> Approve(int proposalId)
+        {
+            ProposalModel proposalModel = await this._service.Approve(proposalId).ConfigureAwait(false);
+            return RedirectToAction("Index", new { conferenceId = proposalModel.ConferenceId });
+        }
+    }
+}
