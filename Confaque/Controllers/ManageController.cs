8c969d63f3ba10a0f83f70ddd5e2e4a10d1721bd
Saurabh Pati <saurabhpati.pati@gmail.com>
2018-01-05 22:44:56 +0530

    Manage Controller

---
+++ Confaque/Controllers/ManageController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Confaque.Data;
+using Shared.Model.Manage;
+
+namespace Confaque.Controllers
+{
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ConfaqueUser> _userManager;
+
+        public ManageController(UserManager<ConfaqueUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ConfaqueUser user = await this._userManager.GetUserAsync(this.User).ConfigureAwait(false);
+            ManageAccountModel model = new ManageAccountModel()
+            {
+                Email = user.Email,
+                Username = user.UserName,
+                IsTwoFactorAuthEnabled = user.TwoFactorEnabled,
+                IsEmailConfirmed = user.EmailConfirmed,
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Shared/Model/Manage/ManageAccountModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Model.Manage
+{
+    public class ManageAccountModel
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
+
+        public bool IsTwoFactorAuthEnabled { get; set; }
+    }
+}
